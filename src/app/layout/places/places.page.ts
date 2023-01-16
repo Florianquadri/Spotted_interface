a2232ca99e0a9fe7f0801fa0fa4146b645fdd2e5
Florianquadri <florianquadri@gmail.com>
2023-01-16 10:14:51 +0100

    linked searchbar with center map

--- src/app/layout/places/places.page.ts
+++ src/app/layout/places/places.page.ts
@@ -8,7 +8,7 @@ import { environment } from "src/environments/environment";
 import { PlacesService } from 'src/app/services/places.service';
 import { Place } from 'src/app/models/place';
 import { FormsModule } from '@angular/forms';
-import { latLng, MapOptions, tileLayer, Map, marker, Marker} from 'leaflet';
+import { latLng, MapOptions, tileLayer, Map, marker, Marker } from 'leaflet';
 import { defaultIcon } from '../../utile_files/default-marker';
 
 import * as L from 'leaflet';
@@ -28,73 +28,74 @@ export class PlacesPage implements ViewWillEnter {
   public places: Place[];
   public results = [];
   public data = [];
-  map : Map;
+  map: Map;
   marker: L.Marker;
   mapMarkers: L.Marker[] = [];
   message = 'This modal example uses the modalController to present and dismiss modals.';
-  
- 
-   constructor( // Inject the authentication provider.
-   private auth: AuthService,
-   private placeService: PlacesService,
-   // Inject the router
-   private router: Router,
-   private http: HttpClient,
-   private modalCtrl: ModalController,
-   ) {
-     this.mapOptions = {
-       layers: [
-         tileLayer(
-           'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-           { maxZoom: 18 }
-         )
-       ],
-       zoom: 13,
-       center: latLng(46.778186, 6.641524)
-     };
+  chosenPlace : Place;
+
+
+  constructor( // Inject the authentication provider.
+    private auth: AuthService,
+    private placeService: PlacesService,
+    // Inject the router
+    private router: Router,
+    private http: HttpClient,
+    private modalCtrl: ModalController,
+  ) {
+    this.mapOptions = {
+      layers: [
+        tileLayer(
+          'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          { maxZoom: 18 }
+        )
+      ],
+      zoom: 13,
+      center: latLng(46.778186, 6.641524)
+    };
+  }
+
+
+  async openModal() {
+    const modal = await this.modalCtrl.create({
+      component: ModalExampleComponent,
+    });
+    modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+
+    if (role === 'confirm') {
+      this.message = `Hello, ${data}!`;
     }
+  }
 
-
-    async openModal() {
-      const modal = await this.modalCtrl.create({
-        component: ModalExampleComponent,
-      });
-      modal.present();
-  
-      const { data, role } = await modal.onWillDismiss();
-  
-      if (role === 'confirm') {
-        this.message = `Hello, ${data}!`;
-      }
-    }
-
-   onMapReady(map: Map) {
+  onMapReady(map: Map) {
     setTimeout(() => map.invalidateSize(), 0);
     this.map = map;
     const center = this.map.getCenter();
     this.locateMe();
     this.getMapCurrentPosition();
-  
+
   }
 
-   addDataToMap() {
+  addDataToMap() {
 
-        for (let i = 0; i < this.data.length; i++) {
-          //const e = this.data[i];
+    for (let i = 0; i < this.data.length; i++) {
+      //const e = this.data[i];
 
-        const marker = L.marker(([this.data[i].location.coordinates[0], this.data[i].location.coordinates[1]]),{icon: defaultIcon}).addTo(this.map);
-       marker.bindPopup(this.data[i].name).openPopup();
- 
-       if (this.marker==this.mapMarkers[i]) {
-        
-       }else{
-  this.mapMarkers.push(marker); 
-       }
-     } 
-      console.log(this.mapMarkers);
-}
+      const marker = L.marker(([this.data[i].location.coordinates[0], this.data[i].location.coordinates[1]]), { icon: defaultIcon }).addTo(this.map);
+      marker.bindPopup(this.data[i].name).openPopup();
 
-   getMapCurrentPosition(){
+      if (this.marker == this.mapMarkers[i]) {
+
+      } else {
+        this.mapMarkers.push(marker);
+      }
+    }
+    console.log(this.mapMarkers);
+  }
+
+  getMapCurrentPosition() {
     this.map.on('locationfound', (e) => {
       const radius = e.accuracy / 2;
 
@@ -103,20 +104,20 @@ export class PlacesPage implements ViewWillEnter {
     }
     );
   }
-   
- 
 
-/*  changeIcon(){
-  this.mapMarkers.forEach(marker => {
-    marker.setIcon(defaultIcon);
-  });
- } */
 
-  
+
+  /*  changeIcon(){
+    this.mapMarkers.forEach(marker => {
+      marker.setIcon(defaultIcon);
+    });
+   } */
+
+
 
   handleChange(event) {
     const query = event.target.value.toLowerCase();
-    if(query==""){
+    if (query == "") {
       this.results = [];
     } else {
       this.results = this.data.filter(d => d.name.toLowerCase().indexOf(query) > -1);
@@ -125,16 +126,24 @@ export class PlacesPage implements ViewWillEnter {
 
   }
 
+  goOnChosenPoint(chosenPlace){
+    console.log(chosenPlace.location.coordinates)
+    this.map.setView(
+      [chosenPlace.location.coordinates[0],
+      chosenPlace.location.coordinates[1]]
+    )
+    this.results = [];
+  }
 
 
-  async locateMe() {  
+  async locateMe() {
     this.addDataToMap();
-    const location = await this.map.locate({setView: true, maxZoom: 16});
+    const location = await this.map.locate({ setView: true, maxZoom: 16 });
 
     this.map.on('locationfound', (e) => {
       const radius = e.accuracy / 2;
-      marker((e.latlng),{icon: defaultIcon}).addTo(this.map)
-          .bindPopup("Vous êtes ici").openPopup();
+      marker((e.latlng), { icon: defaultIcon }).addTo(this.map)
+        .bindPopup("Vous êtes ici").openPopup();
       // circle(e.latlng, radius).addTo(this.map);
     }
     );
@@ -143,36 +152,35 @@ export class PlacesPage implements ViewWillEnter {
       console.log(e);
       alert(e.message);
     });
-    this.addDataToMap();
   }
 
 
-  addMarker(){
-    
+  addMarker() {
+
     this.map.on('click', e => {
-      
+
       this.marker = L.marker(e.latlng).addTo(this.map);
 
       this.mapMarkers.push(this.marker);
-    
+
       this.mapOptions.center = this.marker.getLatLng();
-    this.map.setView( 
-      this.marker.getLatLng(),
-    )
-   /*  this.marker.bindPopup("Nouveau marker").openPopup(); */
+      this.map.setView(
+        this.marker.getLatLng(),
+      )
+      /*  this.marker.bindPopup("Nouveau marker").openPopup(); */
 
-    this.mapMarkers.forEach(marker => {
-      marker.setIcon(defaultIcon);
+      this.mapMarkers.forEach(marker => {
+        marker.setIcon(defaultIcon);
+      });
+      console.log(this.mapMarkers);
+
+      setTimeout(() => {
+        this.openModal();
+      }, 200);
+
+
     });
-    console.log(this.mapMarkers);
 
- setTimeout(() => {
-  this.openModal();
-}, 200); 
-
-
-    });
-  
   }
 
 
@@ -185,15 +193,16 @@ export class PlacesPage implements ViewWillEnter {
       //console.log(places)
       this.places = places;
       this.data = places;
+      this.addDataToMap();
       /* console.log(this.data)
       console.log(this.data[4]) */
-      
+
     })
   }
 
   ngOnInit() {
-  
-    
+
+
   }
 
   logOut() {
